cc51db3c195b55b9d446ea81098768338452dd23
Cheder-one <cherbadenis1@gmail.com>
2023-03-20 03:38:33 +0300

    u

--- src/app/components/typingTrainer.jsx
+++ src/app/components/typingTrainer.jsx
@@ -1,31 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function TypingTrainer() {
-  const [right, setRight] = useState(0);
+  const [currentWord, setCurrentWord] = useState("");
+  const [usedWords, setUsedWords] = useState([]);
+  const [correct, setCorrect] = useState(0);
   const [issue, setIssue] = useState(0);
 
-  const words = ["message", "keyboard", "computer", "programming", "developer"];
+  const words = [
+    "JS",
+    "borderlands",
+    "message",
+    "keyboard",
+    "bob",
+    "coding",
+    "computer",
+    "programming",
+    "developer",
+    "javascript",
+  ];
+  const [availableWords, setAvailableWords] = useState(words);
 
-  let index = 0;
-  const wordTimer = setInterval(() => {
-    console.log(words[index]);
-    index++;
-    if (index === words.length) {
-      clearInterval(wordTimer);
+  const startNewWord = () => {
+    const index = Math.floor(Math.random() * availableWords.length);
+    setCurrentWord(availableWords[index]);
+    setAvailableWords(
+      availableWords.filter((word) => word !== availableWords[index])
+    );
+    setUsedWords((prev) => [...prev, currentWord]);
+  };
+
+  useEffect(() => {
+    const wordsInterval = setInterval(startNewWord, 1000);
+
+    if (usedWords.length > words.length) {
+      clearInterval(wordsInterval);
     }
-  }, 3000);
-
-  document.addEventListener("keyup", (event) => {
-    console.log(event.key);
+    return () => clearInterval(wordsInterval);
   });
+
+  useEffect(() => {
+    document.addEventListener("keyup", (event) => {
+      console.log(event.key);
+    });
+  });
+
+  const numberAttempts = usedWords.length;
+
+  const handleRestart = () => {
+    setAvailableWords(words);
+    setCurrentWord("");
+    setUsedWords([]);
+    setCorrect(0);
+    setIssue(0);
+  };
 
   return (
     <>
       <h1 className="mark">Typing Trainer</h1>
       <div className="m-3">
-        <p className="h5">Правильных слов: {right}</p>
+        <p className="h5">Правильных слов: {correct}</p>
         <p className="h5">Ошибок в словах: {issue}</p>
       </div>
+      {numberAttempts < words.length ? (
+        <div className="h3 m-3">{currentWord}</div>
+      ) : (
+        <>
+          <div className="m-3">
+            Ваш коэффициент верных ответов: {(correct / numberAttempts) * 100}%
+          </div>
+          <button className="m-3" onClick={handleRestart}>
+            Еще раз
+          </button>
+        </>
+      )}
     </>
   );
 }
